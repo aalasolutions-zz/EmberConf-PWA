63f419466b05010b852b1e2bb2d3bfdbbd43d9fc
Aamir Mahmood <aamir@aalasolutions.com>
2021-03-02 23:08:03 +0500

    PWA implemented

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,18 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+
+    'ember-service-worker': {
+      versionStrategy: 'every-build',
+    },
+
+    'esw-cache-fallback': {
+      patterns: [
+        '/mock/(.+)',
+        'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css',
+      ],
+    }
+
   });
 
   // Use `app.import` to add additional libraries to the generated
